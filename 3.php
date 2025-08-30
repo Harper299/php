a02cd32857c672fb0a3ab63848ffd06b61e2449e
Harper299 <masschandru25@gmail.com>
2025-08-30 18:44:31 +0530

    Update 3.php

--- 3.php
+++ 3.php
@@ -2,17 +2,18 @@
 function valid_email($email)
 {
     $result = trim($email);
-    if (filter_var($result, FILTER_VALIDATE_EMAIL)) {
-        return "Valid Email";
-    } else {
-        return "Invalid Email";
+    if (filter_var($result,FILTER_VALIDATE_EMAIL))
+    {
+        return "- Valid Email";
+    }
+    else
+    {
+        return "- Invalid Email";
     }
 }
-
-// Driver code
-$emails = ["abc@example.com", "abc#example.com"];
-
-foreach ($emails as $email) {
-    echo $email . "  " . valid_email($email) . "<br>";
-}
+//Driver code
+echo "abc@example.com ";
+echo valid_email("abc@example.com")."<br>"; 
+echo "abc#example.com ";
+echo valid_email("abc#example.com")."<br>";
 ?>

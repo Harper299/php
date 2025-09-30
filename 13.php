912fc33d5d83b29051d6c9956eb3f6c9319d6035
Harper299 <masschandru25@gmail.com>
2025-09-30 19:08:14 +0530

    Update 13.php

--- 13.php
+++ 13.php
@@ -21,21 +21,21 @@
 File name 2 : tree14.php
 <?php
 echo "Three different ways to retrieve name John <br>";
-// initialize new DOMDocument
+
 $doc = new DOMDocument();
-// disable whitespace-only text nodes
+
 $doc->preserveWhiteSpace = false;
-// read XML file
+
 $doc->load('tree14.xml');
-// output: 'John'
+
 echo $doc->firstChild->childNodes->item(2)->childNodes
 ->item(2)->childNodes->item(0)->childNodes->item(0)
 ->childNodes->item(0)->nodeValue."<br>";
-// output: 'John'
+
 echo
 $doc->getElementsByTagName('name')->item(0)->nodeValue."<br>"
 ;
-// output: 'John'
+
 echo $doc->getElementsByTagName('person')->item(4)
 ->childNodes->item(0)->nodeValue."<br>";
 ?>
